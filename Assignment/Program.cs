ce31f8a601ae67ea70fbdbab02741285a3d5b4ca
A00241382@mycambrian.ca <123224843+rebeccaYMCG@users.noreply.github.com>
2023-06-07 16:22:15 -0400

    Test Program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main()
         {
+            System.Console.WriteLine("Hello World");
             // Run your RobotTester class here -> RobotTester.TestRobot()
         }
     }
