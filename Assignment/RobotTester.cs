2966ecbf061f01acbb87f27701f356fb20712c20
A00241382@mycambrian.ca <123224843+rebeccaYMCG@users.noreply.github.com>
2023-06-12 16:08:51 -0400

    Update Format

--- Assignment/RobotTester.cs
+++ Assignment/RobotTester.cs
@@ -55,7 +55,7 @@ namespace RobotTester
                         continue;
                 }
 
-                 commands.Add(command);
+                commands.Add(command);
             }
 
             Console.WriteLine();
